4a63d702dbcbb3b2a9dc3d92aabe8c3aae4098a5
Jal Devecais <chiyokogaming02@gmail.com>
2024-06-07 10:12:40 +0800

    deleted img

--- dbeventscheduler.sql
+++ dbeventscheduler.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 30, 2024 at 05:54 PM
+-- Generation Time: Jun 07, 2024 at 04:08 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -24,15 +24,86 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_allevents`
+--
+
+CREATE TABLE `tbl_allevents` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `timefrom` time NOT NULL,
+  `timeto` time NOT NULL,
+  `venue` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_approvedevents`
+--
+
+CREATE TABLE `tbl_approvedevents` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `timefrom` time NOT NULL,
+  `timeto` time NOT NULL,
+  `venue` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_cancelledevents`
+--
+
+CREATE TABLE `tbl_cancelledevents` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `timefrom` time NOT NULL,
+  `timeto` time NOT NULL,
+  `venue` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_deletedevents`
+--
+
+CREATE TABLE `tbl_deletedevents` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `timefrom` time NOT NULL,
+  `timeto` time NOT NULL,
+  `venue` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_events`
 --
 
 CREATE TABLE `tbl_events` (
   `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `timefrom` time NOT NULL,
+  `timeto` time NOT NULL,
   `venue` varchar(255) NOT NULL,
-  `time` varchar(255) NOT NULL,
-  `date` varchar(255) NOT NULL
+  `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -65,12 +136,49 @@ CREATE TABLE `tbl_login` (
 --
 
 INSERT INTO `tbl_login` (`id`, `username`, `password`) VALUES
-(1, 'poorsaken', 'poorsaken'),
-(2, 'admin', '$2y$10$sgQSE/JL9rc5odM8Yew/0uIxidPnvOWT3Y91QctFpb6IvDm4ri1QS');
+(10, 'angelica12', '$2y$10$dEruvD0x3jLq8wMPpoqaK.TwemA1ZF8XOBakobDih0gNpn4r/Luyi'),
+(11, 'bokybox', '$2y$10$iQi8T7PN/I4E931FUhfk6.FX40IM6NCTIqBoldMyz2LWXE0bK3oSC'),
+(13, '#Angel1ca@FueNtebella', '$2y$10$jiXbPgEeWRuIx/C.eHAC/O5Z0lhJ.lHDwF3OtCvPtAezkVqBQ69P.'),
+(14, 'jalstore', '$2y$10$0oBHp.Tz8FRSUrq.FyycRuKux850bkBDkpVBF78rJDxWF/FQAVZom');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_venue`
+--
+
+CREATE TABLE `tbl_venue` (
+  `id` int(11) NOT NULL,
+  `venue` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tbl_allevents`
+--
+ALTER TABLE `tbl_allevents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_approvedevents`
+--
+ALTER TABLE `tbl_approvedevents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_cancelledevents`
+--
+ALTER TABLE `tbl_cancelledevents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_deletedevents`
+--
+ALTER TABLE `tbl_deletedevents`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tbl_events`
@@ -91,14 +199,44 @@ ALTER TABLE `tbl_login`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tbl_venue`
+--
+ALTER TABLE `tbl_venue`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `tbl_allevents`
+--
+ALTER TABLE `tbl_allevents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_approvedevents`
+--
+ALTER TABLE `tbl_approvedevents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_cancelledevents`
+--
+ALTER TABLE `tbl_cancelledevents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_deletedevents`
+--
+ALTER TABLE `tbl_deletedevents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tbl_events`
 --
 ALTER TABLE `tbl_events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `tbl_host`
@@ -110,7 +248,13 @@ ALTER TABLE `tbl_host`
 -- AUTO_INCREMENT for table `tbl_login`
 --
 ALTER TABLE `tbl_login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `tbl_venue`
+--
+ALTER TABLE `tbl_venue`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
